242a152eef062dc272e81169ae834dc22dff4e9b
bwilson1802 <110945068+bwilson1802@users.noreply.github.com>
2022-09-20 14:18:28 -0400

    IP update

--- main.tf
+++ main.tf
@@ -28,34 +28,9 @@ resource "azurerm_resource_group" "Demo-RG" {
   location = "East US"
 }
 
-##########################
-# RTR VM                 #
-##########################
-
-resource "azurerm_windows_virtual_machine" "RTR" {
-  name                = "RTR-VM"
-  resource_group_name = azurerm_resource_group.Demo-RG.name
-  location            = azurerm_resource_group.Demo-RG.location
-  size                = "Standard_B2s"
-  admin_username      = "adminuser"
-  admin_password      = "1qaz2wsx!QAZ@WSX"
-  network_interface_ids = [azurerm_network_interface.Demo-Network.id, azurerm_network_interface.LON-Subnet.id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "StandardSSD_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
-    version   = "latest"
-  }
-}
 
 ##########################
-# RTR network breakout   #
+# network breakout       #
 ##########################
 
 # RTR LON-RTR-NIC 10.10.1.1, NA-RTR-NIC 10.10.2.1, PA-RTR-NIC 10.10.3.1, RTR-Internet
@@ -86,8 +61,80 @@ resource "azurerm_network_security_group" "Demo-Sec-Group" {
   }
 }
 
+##########
+
+resource "azurerm_public_ip" "RTR-Pub-Network" {
+  name                = "RTR-Pub-Network"
+  resource_group_name = azurerm_resource_group.Demo-RG.name
+  location            = azurerm_resource_group.Demo-RG.location
+  allocation_method   = "Dynamic"
+    
+  tags = {
+    environment = "Dev"
+  }
+}
+
 ###################
-# RTR - Internet  #
+# Subnet          #
+###################
+
+resource "azurerm_subnet" "RTR-Subnet" {
+  name                 = "RTR-subnet"
+  resource_group_name  = azurerm_resource_group.Demo-RG.name
+  virtual_network_name = azurerm_virtual_network.Demo-Network.name
+  address_prefixes     = ["10.10.0.192/26"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "RTR-Subnet-Sec-Group" {
+  subnet_id                 = azurerm_subnet.RTR-Subnet.id
+  network_security_group_id = azurerm_network_security_group.Demo-Sec-Group.id
+}
+
+############
+
+resource "azurerm_subnet" "LON-Subnet" {
+  name                 = "LON-subnet"
+  resource_group_name  = azurerm_resource_group.Demo-RG.name
+  virtual_network_name = azurerm_virtual_network.Demo-Network.name
+  address_prefixes     = ["10.10.0.0/26"]
+  
+}
+
+resource "azurerm_subnet_network_security_group_association" "LON-Subnet-Sec-Group" {
+  subnet_id                 = azurerm_subnet.LON-Subnet.id
+  network_security_group_id = azurerm_network_security_group.Demo-Sec-Group.id
+}
+
+########
+
+resource "azurerm_subnet" "NA-Subnet" {
+  name                 = "NA-subnet"
+  resource_group_name  = azurerm_resource_group.Demo-RG.name
+  virtual_network_name = azurerm_virtual_network.Demo-Network.name
+  address_prefixes     = ["10.10.0.64/26"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "NA-Subnet-Sec-Group" {
+  subnet_id                 = azurerm_subnet.NA-Subnet.id
+  network_security_group_id = azurerm_network_security_group.Demo-Sec-Group.id
+}
+
+########
+
+resource "azurerm_subnet" "PAC-Subnet" {
+  name                 = "PAC-subnet"
+  resource_group_name  = azurerm_resource_group.Demo-RG.name
+  virtual_network_name = azurerm_virtual_network.Demo-Network.name
+  address_prefixes     = ["10.10.0.128/26"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "PAC-Subnet-Sec-Group" {
+  subnet_id                 = azurerm_subnet.PAC-Subnet.id
+  network_security_group_id = azurerm_network_security_group.Demo-Sec-Group.id
+}
+
+###################
+# NIC             #
 ###################
 
 resource "azurerm_network_interface" "RTR-Pub-NIC" {
@@ -110,28 +157,13 @@ resource "azurerm_application_security_group" "RTR-appsecuritygroup" {
   name                = "RTR-appsecuritygroup"
   location            = azurerm_resource_group.Demo-RG.location
   resource_group_name = azurerm_resource_group.Demo-RG.name
-  depends_on = [azurerm_network_interface.RTR-Pub-NIC]
-  
+
   tags = {
     environment = "Dev"
   }
 }
 
-resource "azurerm_public_ip" "RTR-Pub-Network" {
-  name                = "RTR-Pub-Network"
-  resource_group_name = azurerm_resource_group.Demo-RG.name
-  location            = azurerm_resource_group.Demo-RG.location
-  allocation_method   = "Dynamic"
-  depends_on = [azurerm_network_interface.RTR-Pub-NIC]
-  
-  tags = {
-    environment = "Dev"
-  }
-}
-
-###################
-# RTR-LON         #
-###################
+########## 
 
 resource "azurerm_network_interface" "RTR-LON-NIC" {
   name                = "RTR-LON-NIC"
@@ -142,8 +174,9 @@ resource "azurerm_network_interface" "RTR-LON-NIC" {
     name                          = "RTR-LON-Network"
     subnet_id                     = azurerm_subnet.LON-Subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.Demo-Network.id
+    public_ip_address_id          = azurerm_public_ip.RTR-Pub-Network.id
   }
+  
   tags = {
     enviornment = "dev"
   }
@@ -153,43 +186,13 @@ resource "azurerm_application_security_group" "RTR-LON-appsecuritygroup" {
   name                = "RTR-LON-appsecuritygroup"
   location            = azurerm_resource_group.Demo-RG.location
   resource_group_name = azurerm_resource_group.Demo-RG.name
-  depends_on = [azurerm_network_interface.RTR-LON-NIC]
-  
+    
   tags = {
     environment = "Dev"
   }
 }
 
-resource "azurerm_subnet" "LON-Subnet" {
-  name                 = "LON-subnet"
-  resource_group_name  = azurerm_resource_group.Demo-RG.name
-  virtual_network_name = azurerm_virtual_network.Demo-Network.name
-  address_prefixes     = ["10.10.1.0/26"]
-  depends_on = [azurerm_network_interface.RTR-LON-NIC]
-}
-
-resource "azurerm_network_security_group" "RTR-LON-Sec-Group" {
-  name                = "RTR-LON-Sec-Group"
-  location            = azurerm_resource_group.Demo-RG.location
-  resource_group_name = azurerm_resource_group.Demo-RG.name
-  depends_on = [azurerm_network_interface.RTR-LON-NIC]
-
-  security_rule {
-    name                       = "test123"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-###################
-# RTR-NA          #
-###################
+##########
 
 resource "azurerm_network_interface" "RTR-NA-NIC" {
   name                = "RTR-NA-NIC"
@@ -200,8 +203,9 @@ resource "azurerm_network_interface" "RTR-NA-NIC" {
     name                          = "RTR-NA-Network"
     subnet_id                     = azurerm_subnet.NA-Subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.Demo-Network.id
+    public_ip_address_id          = azurerm_public_ip.RTR-Pub-Network.id
   }
+  
   tags = {
     enviornment = "dev"
   }
@@ -211,43 +215,13 @@ resource "azurerm_application_security_group" "RTR-NA-appsecuritygroup" {
   name                = "RTR-NA-appsecuritygroup"
   location            = azurerm_resource_group.Demo-RG.location
   resource_group_name = azurerm_resource_group.Demo-RG.name
-  depends_on = [azurerm_network_interface.RTR-NA-NIC]
-  
+    
   tags = {
     environment = "Dev"
   }
 }
 
-resource "azurerm_subnet" "NA-Subnet" {
-  name                 = "NA-subnet"
-  resource_group_name  = azurerm_resource_group.Demo-RG.name
-  virtual_network_name = azurerm_virtual_network.Demo-Network.name
-  address_prefixes     = ["10.10.2.0/26"]
-  depends_on = [azurerm_network_interface.RTR-NA-NIC]
-}
-
-resource "azurerm_network_security_group" "RTR-NA-Sec-Group" {
-  name                = "RTR-NA-Sec-Group"
-  location            = azurerm_resource_group.Demo-RG.location
-  resource_group_name = azurerm_resource_group.Demo-RG.name
-  depends_on = [azurerm_network_interface.RTR-NA-NIC]
-
-  security_rule {
-    name                       = "test123"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-###################
-# RTR-PAC          #
-###################
+##########
 
 resource "azurerm_network_interface" "RTR-PAC-NIC" {
   name                = "RTR-PAC-NIC"
@@ -258,8 +232,9 @@ resource "azurerm_network_interface" "RTR-PAC-NIC" {
     name                          = "RTR-PAC-Network"
     subnet_id                     = azurerm_subnet.PAC-Subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.Demo-Network.id
+    public_ip_address_id          = azurerm_public_ip.RTR-Pub-Network.id
   }
+  
   tags = {
     enviornment = "dev"
   }
@@ -269,38 +244,34 @@ resource "azurerm_application_security_group" "RTR-PAC-appsecuritygroup" {
   name                = "RTR-PAC-appsecuritygroup"
   location            = azurerm_resource_group.Demo-RG.location
   resource_group_name = azurerm_resource_group.Demo-RG.name
-  depends_on = [azurerm_network_interface.RTR-PAC-NIC]
   
   tags = {
     environment = "Dev"
   }
 }
 
-resource "azurerm_subnet" "PAC-Subnet" {
-  name                 = "PAC-subnet"
-  resource_group_name  = azurerm_resource_group.Demo-RG.name
-  virtual_network_name = azurerm_virtual_network.Demo-Network.name
-  address_prefixes     = ["10.10.3.0/26"]
-  depends_on = [azurerm_network_interface.RTR-PAC-NIC]
-}
+##########################
+# RTR VM                 #
+##########################
 
-resource "azurerm_network_security_group" "RTR-PAC-Sec-Group" {
-  name                = "RTR-PAC-Sec-Group"
-  location            = azurerm_resource_group.Demo-RG.location
+resource "azurerm_windows_virtual_machine" "RTR" {
+  name                = "RTR-VM"
   resource_group_name = azurerm_resource_group.Demo-RG.name
-  depends_on = [azurerm_network_interface.RTR-PAC-NIC]
+  location            = azurerm_resource_group.Demo-RG.location
+  size                = "Standard_B2s"
+  admin_username      = "adminuser"
+  admin_password      = "1qaz2wsx!QAZ@WSX"
+  network_interface_ids = [azurerm_network_interface.RTR-Pub-NIC.id, azurerm_network_interface.RTR-NA-NIC.id, azurerm_network_interface.RTR-PAC-NIC.id, azurerm_network_interface.RTR-LON-NIC.id]
 
-  security_rule {
-    name                       = "test123"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
   }
 }
-
-
